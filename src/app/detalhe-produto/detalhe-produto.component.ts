8c4ee3aac9a7588d3a3c6d4c27be5d4682b1c014
Trojahn <tiagotrojahn@ifsp.edu.br>
2022-04-07 01:27:14 +0000

    Adicionado Service e modificado componente para utilizá-lo

--- src/app/detalhe-produto/detalhe-produto.component.ts
+++ src/app/detalhe-produto/detalhe-produto.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { DatabaseService } from '../database.service';
 import { Produto } from '../model/produto.model';
 
 @Component({
@@ -6,20 +7,27 @@ import { Produto } from '../model/produto.model';
   templateUrl: './detalhe-produto.component.html',
   styleUrls: ['./detalhe-produto.component.css']
 })
-export class DetalheProdutoComponent implements OnInit {
+export class DetalheProdutoComponent implements OnInit, OnChanges {
 
-  @Input() item : Produto;
+  @Input() item : number;
+  produto : Produto;
   @Output() fecharComponente = new EventEmitter<String>();
 
 
-  constructor() { }
+  constructor(private data : DatabaseService) { }
 
   ngOnInit(): void {
-
+    this.produto = this.data.getProduto(this.item);
    }
 
+   ngOnChanges(changes: SimpleChanges): void {
+    this.produto = this.data.getProduto(this.item);
+   }
+
+
+
   fechar() : void {
-    this.fecharComponente.emit(`Fechando o produto '${this.item.name}'`);
+    this.fecharComponente.emit(`Fechando o produto '${this.produto.name}'`);
   }
 
 }
--- src/app/lista-produtos/lista-produtos.component.ts
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
 import { Produto } from '../model/produto.model';
 
 @Component({
@@ -7,28 +8,28 @@ import { Produto } from '../model/produto.model';
   styleUrls: ['./lista-produtos.component.css']
 })
 export class ListaProdutosComponent implements OnInit {
-  produtoSelecionado : Produto;
+  produtoSelecionado : Number;
   
-  lista : Produto[] = [
-    {name: "Toddynho", price: 9.99, qtde: 2000},
-    {name: "Ovo", price: 8.99, qtde: 255},
-    {name: "Leite", price: 3.99, qtde: 2550},
-    {name: "Máscara", price: 6.99, qtde: 2550},
-    {name: "Polenta frita", price: 16.99, qtde: 400},
-  ]
+  lista : Produto[];
 
-  constructor() { }
+  constructor(private data : DatabaseService) { }
 
   ngOnInit(): void {
+    this.atualizarProdutos();
+    this.produtoSelecionado = -1;
+  }
+
+  private atualizarProdutos() {
+    this.lista = this.data.getProdutos();
   }
 
   executarFechamento(info : String) {
     console.log(info);
-    this.produtoSelecionado = null;
+    this.produtoSelecionado = -1;
   }
 
-  selecionar(produto : Produto) : void {
-    this.produtoSelecionado = produto;
+  selecionar(indice : Number) : void {
+    this.produtoSelecionado = indice;
   }
 
 }
